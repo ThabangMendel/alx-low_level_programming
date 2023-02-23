6d95fd7db3d47b90587afbfe4130d3ab4b2e77a7
ThabangMendel <thabanghendrickm@gmail.com>
2023-02-23 12:35:00 -0800

    Task 5 added to the repository

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	char base_10[] = "0123456789";
+
+	printf("%s\n", base_10);
+
+	return (0);
+}
