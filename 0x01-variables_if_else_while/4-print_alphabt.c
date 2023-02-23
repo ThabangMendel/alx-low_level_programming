b9bcb5aeb3fd74e73410ec14d7939345d463d80c
ThabangMendel <thabanghendrickm@gmail.com>
2023-02-23 11:08:58 -0800

    Task 4 added to the repository

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 97; letter < 123; letter++)
+	{
+		if (letter != 101 && letter != 123)
+		{
+			putchar(letter);
+		}
+	}
+	putchar(10);
+	return (0);
+}
