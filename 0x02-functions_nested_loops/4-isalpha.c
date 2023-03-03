763b08fbaef659612c2f5a88551a3febce6be95f
ThabangMendel <thabanghendrickm@gmail.com>
2023-03-03 14:46:07 -0800

    Fixed line exceeding limit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphatic characters
- * @c: character to be checked
- * Return: 1 if character is a letter that's either lower or uppercase, 0 otherwise
+ * _isalpha - checks for alphatic characters.
+ * @c: character to be checked.
+ * Return: 1 if character is a letter that's either lower or
+ *  uppercase, 0 otherwise.
  */
 int _isalpha(int c)
 {
