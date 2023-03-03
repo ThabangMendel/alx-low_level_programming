288a5700de48d85de67091e0480952266a69639f
ThabangMendel <thabanghendrickm@gmail.com>
2023-03-03 14:43:07 -0800

    Task 4 added to the repo

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphatic characters
+ * @c: character to be checked
+ * Return: 1 if character is a letter that's either lower or uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 &&  c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
