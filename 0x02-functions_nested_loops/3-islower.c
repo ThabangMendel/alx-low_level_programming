cea9fae2d204d82216f35d38b3e59aa6ca0f3aa4
ThabangMendel <thabanghendrickm@gmail.com>
2023-03-03 14:12:20 -0800

    Fixed betty documentation style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * islower - checks for lowercase character
- * Returns: (1) if  @c is lowercase
- * Returns: (0) otherwise
+ * _islower - checks for lowercase character
+ * @c: Character to be checked 
+ * Returns: (1) if character is lowercase , (0) otherwise
  */
 int _islower(int c)
 {
-	if ( c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
